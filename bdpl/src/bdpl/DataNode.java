9439466ecb9923fc9a0ac49e1999f49416770fe8
akshaypundle <akshaypundle@7d29676a-293b-0410-9221-f59a76fd9467>
2007-11-08 16:32:45 +0000

    Non working code !!

---
+++ bdpl/src/bdpl/DataNode.java
@@ -0,0 +1,25 @@
+/*
+ * DataNode.java
+ *
+ * Created on November 8, 2007, 9:43 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ * This defines the interface which all data nodes should 
+ * adhere to (i.e this is the abstract base class for all
+ * our concrete data node classes 
+ *
+ * @author akshay
+ */
+public interface DataNode
+{
+    public String get_type_name();
+    public String get_bitsequence_value() ;
+    public int get_bit_size(); 
+    public int get_int_value();
+    public int get_fieldsize();
+    public int set_fieldsize();
+}
---
+++ bdpl/src/bdpl/DataNodeStruct.java
@@ -0,0 +1,28 @@
+/*
+ * DataNodeStruct.java
+ *
+ * Created on November 8, 2007, 10:04 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akshay
+ */
+public class DataNodeStruct
+{
+    
+    /** Creates a new instance of DataNodeStruct */
+    public DataNodeStruct ()
+    {
+    }
+        public String get_type_name() {return "";}
+    public String get_bitsequence_value() {return "";}
+    public int get_bit_size()  {return 0;}
+    public int get_int_value() {return 0;}
+    public int get_fieldsize() {return 0;}
+    public int set_fieldsize() {return 0;}
+    
+}
