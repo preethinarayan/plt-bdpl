47722d7217ebfcc7ee1043bec89f58683c23b0a5
vr.bharadwaj <vr.bharadwaj@7d29676a-293b-0410-9221-f59a76fd9467>
2007-11-14 03:11:14 +0000

    Test code for AST annotation in grammar file.

--- trunk/bdpl/src/bdpl/BdplMain.java
+++ trunk/bdpl/src/bdpl/BdplMain.java
@@ -1,11 +1,20 @@
 import java.io.*;
+import antlr.CommonAST;
+import antlr.collections.AST;
+import antlr.debug.misc.ASTFrame;
 
 public class BdplMain{
     public static void main(String[] args){
 	try{
-	    BdplLexer lexer = new BdplLexer(new FileInputStream("../../Users/akshay/Documents/school/plt/trunk/bdpl/test/prog3.bdl"));
+	    BdplLexer lexer = new BdplLexer(new FileInputStream("x:/test/prog3.bdl"));
 	    BdplParser parser = new BdplParser(lexer);
 	    parser.file();
+            
+            CommonAST parseTree = (CommonAST)parser.getAST();
+            System.out.println(parseTree.toStringList());
+            
+            ASTFrame frame = new ASTFrame("AST for the BDPL Parser", parseTree);
+            frame.setVisible(true);
 	}catch(Exception e){
 	    System.err.println("Exception: "+e);
         }
