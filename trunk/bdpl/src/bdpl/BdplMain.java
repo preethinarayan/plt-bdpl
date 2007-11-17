07878ba5476c71d81fbeb1aee1a6733b03bc1a1b
vr.bharadwaj <vr.bharadwaj@7d29676a-293b-0410-9221-f59a76fd9467>
2007-11-17 23:33:48 +0000

    The framework of the tree walker is introduced.

--- trunk/bdpl/src/bdpl/BdplMain.java
+++ trunk/bdpl/src/bdpl/BdplMain.java
@@ -8,13 +8,16 @@ public class BdplMain{
 	try{
 	    BdplLexer lexer = new BdplLexer(new FileInputStream("x:/test/prog3.bdl"));
 	    BdplParser parser = new BdplParser(lexer);
-	    parser.file();
+	    parser.program();
             
             CommonAST parseTree = (CommonAST)parser.getAST();
             System.out.println(parseTree.toStringList());
             
             ASTFrame frame = new ASTFrame("AST for the BDPL Parser", parseTree);
             frame.setVisible(true);
+
+	    BdplTreeParser treeParser = new BdplTreeParser();
+            treeParser.program(parseTree);
 	}catch(Exception e){
 	    System.err.println("Exception: "+e);
         }
