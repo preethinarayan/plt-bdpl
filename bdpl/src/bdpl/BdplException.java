04e65a2623e7718457e83c0caa38943e02d06110
vr.bharadwaj <vr.bharadwaj@7d29676a-293b-0410-9221-f59a76fd9467>
2007-12-11 10:45:19 +0000

    New class to collect and handle exceptions

---
+++ bdpl/src/bdpl/BdplException.java
@@ -0,0 +1,22 @@
+/*
+ * BdplException.java
+ *
+ * Created on December 9, 2007, 8:01 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+import java.util.*;
+/**
+ *
+ * @author Bharadwaj
+ */
+public class BdplException extends RuntimeException {
+    
+    /** Creates a new instance of BdplException */
+    public BdplException(String s){
+        System.out.println(s);
+    }
+    
+}
