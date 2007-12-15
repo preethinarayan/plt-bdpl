9cef4b8620c61456d395a6475ab4a22ff2f2a614
akshaypundle <akshaypundle@7d29676a-293b-0410-9221-f59a76fd9467>
2007-12-15 09:27:19 +0000

    fixed bug 10 i.e : input : struct outer { 	byte size; 	struct outer 	{ 		byte[size/2] n_bytes; 	}[*] b; }[*] c;
    file "x:/test/file_test.dat" inp_file;
    read(inp_file,c);
    print(c[0].b);
    
    
    output :
    ARRAY of struct:outer
    [
    ]

--- trunk/bdpl/src/bdpl/DataNodeArray.java
+++ trunk/bdpl/src/bdpl/DataNodeArray.java
@@ -96,16 +96,11 @@ public class DataNodeArray extends DataNodeAbstract
             else if(_dummy.getClass ().getCanonicalName ().equals ("DataNodeStruct"))
             {
                 child=new DataNodeStruct ( (DataNodeStruct) _dummy  );
-                VariableSymbolTable v=new VariableSymbolTable();
-                TypeSymbolTable t=new TypeSymbolTable();
-                v.set_parent (((DataNodeStruct) _dummy).get_scope_var ());
-                t.set_parent (((DataNodeStruct) _dummy).get_scope_type ());
-                ((DataNodeStruct)child).set_scope (v,t);
+                ((DataNodeStruct) child).get_scope_var ().set_parent (_scope_var_pointer);
             }
             try
             {
                 _children.add (child);
-                //append_element (child);
             }
             catch( Exception e)
             {
