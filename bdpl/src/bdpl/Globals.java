693c53c014cd88fe5de20e8d56437ab69e9b306c
akshaypundle <akshaypundle@7d29676a-293b-0410-9221-f59a76fd9467>
2007-12-16 09:26:34 +0000

    initial version

---
+++ bdpl/src/bdpl/Globals.java
@@ -0,0 +1,22 @@
+/*
+ * Globals.java
+ *
+ * Created on December 15, 2007, 10:16 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akshay
+ */
+public class Globals
+{
+    public Globals()
+    {
+        little_endian=true;
+    }
+    /** is everything supposed to be done in little endian ? */
+    public static boolean little_endian;
+}
