eee44b0dd770c594c0397a981229a4e401549c25
akshaypundle <akshaypundle@7d29676a-293b-0410-9221-f59a76fd9467>
2007-12-16 09:26:34 +0000

    initial version

---
+++ trunk/bdpl/src/bdpl/BdplMemFile.java
@@ -0,0 +1,22 @@
+/*
+ * BdplMemFile.java
+ *
+ * Created on December 16, 2007, 3:54 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akshay
+ */
+public class BdplMemFile
+{
+    
+    /** Creates a new instance of BdplMemFile */
+    public BdplMemFile ()
+    {
+    }
+    
+}
