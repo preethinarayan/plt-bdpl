8da52b83f9ea03710685373eb5732f45035fd972
vr.bharadwaj <vr.bharadwaj@7d29676a-293b-0410-9221-f59a76fd9467>
2007-12-16 23:37:13 +0000

    Creating reference output files. Also changed BdplMain so that AST frame is not displayed

--- trunk/bdpl/src/bdpl/BdplMain.java
+++ trunk/bdpl/src/bdpl/BdplMain.java
@@ -161,8 +161,8 @@ public class BdplMain
             CommonAST parseTree = (CommonAST)parser.getAST();
             //System.out.println(parseTree.toStringList());
             
-            ASTFrame frame = new ASTFrame("AST for the BDPL Parser", parseTree);
-            frame.setVisible(true);
+//            ASTFrame frame = new ASTFrame("AST for the BDPL Parser", parseTree);
+//            frame.setVisible(true);
 
 	    BdplTreeParser treeParser = new BdplTreeParser();
             treeParser.program(parseTree);           
--- trunk/bdpl/test/test.tcl
+++ trunk/bdpl/test/test.tcl
@@ -5,12 +5,14 @@ set env(CLASSPATH) {x:\tools\antlr.jar;x:\lib\bdpl.jar}
 puts $env(CLASSPATH)
 
 set testPath [file nativename [file join "." "test"]]
-puts $testPath
 cd $testPath
 
 # Collect all the bdl files that are in the current directory
 set files [glob *.bdl]
 
 # Loop through all the files that have been found
-foreach f $files { puts "Testing $f"
-set result [exec -- $javaexec BdplMain $f]}
+foreach f $files { 
+ set result [exec -- $javaexec BdplMain $f 2>&1 > $f.out]
+ catch {set result [exec -- diff -b -q $f.out $f.golden ]} error
+ switch $error "" "puts {Testing $f......OK}" "default" "puts {Testing $f .......FAILED}"
+}
